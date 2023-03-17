aab821d6c7afc7c49187d699aac22ca1f0aefe28
joanna <j.baranowska@hotmail.com>
2023-03-17 12:13:56 +0100

    render bucket

--- core/src/com/potter/Drop.java
+++ core/src/com/potter/Drop.java
@@ -4,9 +4,12 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.awt.*;
 
 public class Drop extends ApplicationAdapter {
 
@@ -14,6 +17,9 @@ public class Drop extends ApplicationAdapter {
 	private Texture bucketImage;
 	private Sound dropSound;
 	private Music rainMusic;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Rectangle bucket;
 
 	@Override
 	public void create () {
@@ -25,11 +31,29 @@ public class Drop extends ApplicationAdapter {
 
 		rainMusic.setLooping(true);
 		rainMusic.play();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, GUIParams.WIDTH, GUIParams.HEIGHT);
+
+		batch = new SpriteBatch();
+
+		bucket = new Rectangle();
+		bucket.x = GUIParams.WIDTH / 2 - GUIParams.ENTITY_SIZE / 2;
+		bucket.y = 20;
+		bucket.width = GUIParams.ENTITY_SIZE;
+		bucket.height = GUIParams.ENTITY_SIZE;
 	}
 
 	@Override
 	public void render () {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
 
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		batch.end();
 	}
 	
 	@Override
---
+++ core/src/com/potter/GUIParams.java
@@ -0,0 +1,7 @@
+package com.potter;
+
+public class GUIParams {
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 480;
+    public static final int ENTITY_SIZE = 64;
+}
