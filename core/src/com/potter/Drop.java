4d7c2726f44459fa2c8ddf4906d35bae3b8543b7
joanna <j.baranowska@hotmail.com>
2023-03-17 11:59:13 +0100

    added base files

--- core/src/com/potter/Drop.java
+++ core/src/com/potter/Drop.java
@@ -1,15 +1,30 @@
 package com.potter;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Drop extends ApplicationAdapter {
 
+	private Texture dropImage;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+
 	@Override
 	public void create () {
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
 
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		rainMusic.setLooping(true);
+		rainMusic.play();
 	}
 
 	@Override
